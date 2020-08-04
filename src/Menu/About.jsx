10f2694269b1f7502c09d37915e4037418c6b3a4
gimsang-won <ksw75811@gmail.com>
2020-08-04 19:30:55 +0900

    로고 수정

--- src/Menu/About.jsx
+++ src/Menu/About.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import {Myprofile, Mainprofile} from './Mystyle';
 
 export const About = () => {
-    return(<div>
+    return (<div>
         <Myprofile>
             <div style={{
                 fontSize: '24px',
                 fontWeight: 'bold',
-            }}>김상원 1991년 7월 18일 생</div>
-            <div>다양한 기능을 가진 예쁜 웹페이지를 만들어보자.</div>
+            }}>김상원 1991년 7월 18일 생
+            </div>
+            <div>모든 현상의 근원에 대해서 철학적으로 접근하는 개발자가 되고자 합니다.</div>
         </Myprofile>
         <Mainprofile>
             <div>
@@ -18,19 +19,21 @@ export const About = () => {
                 <div>FM테크 풀스택 웹 개발자 (2018.05 ~ 2020.04)</div>
                 <div>(현) 생각솜씨 개발 총괄(ReactJS, Styled-Component, Github, Webcrawling)</div>
                 <div>(현) 김상원의 웹페이지 개발 총괄(ReactJS, Styled-Component, NodeJS, MongoDB, AWS)</div>
-                <div>직장인 김상원이 아닌 프리랜서 사업가 김상원으로 살아가려고 노력 중입니다.</div>
+                <div>(현) 유튜브 채널 개설 중(기획단계)</div>
             </div>
             <div>
                 <p>포트폴리오</p>
                 <div>Reverse Escape 방탈출 카페 홈페이지 개발 (퍼블리싱, 프론트엔드, 백엔드) (2018.12)</div>
-                <div>홈페이지 : <a  target="_blank" href ='https://www.reverseescape.com/'>https://www.reverseescape.com</a></div>
+                <div>홈페이지 : <a target="_blank" href='https://www.reverseescape.com/'>https://www.reverseescape.com</a>
+                </div>
                 <div>HappyInside 홈페이지 개발 (프론트엔드, 백엔드) (2019.04)</div>
-                <div>홈페이지 : <a  target="_blank" href ='http://happyinside.co.kr/'>http://happyinside.co.kr</a></div>
+                <div>홈페이지 : <a target="_blank" href='http://happyinside.co.kr/'>http://happyinside.co.kr</a></div>
                 <div>안양 다락방 방탈출 카페 (퍼블리싱, 프론트엔드, 백엔드) (2020.04)</div>
-                <div>홈페이지 : <a  target="_blank" href ='http://www.darockesc.com/'>http://www.darockesc.com</a></div>
+                <div>홈페이지 : <a target="_blank" href='http://www.darockesc.com/'>http://www.darockesc.com</a></div>
                 <div>수원 리멤버 방탈출 카페 (퍼블리싱, 프론트엔드, 백엔드) (2020.5)</div>
-                <div>홈페이지 : <a  target="_blank" href ='http://rememberesc.com/'>http://rememberesc.com</a></div>
-                <div>생각솜씨 (국제 기억력 테스트 홈페이지 제작 중) (퍼블리싱, 프론트엔드, 백엔드) (현재 개발중)</div>
+                <div>홈페이지 : <a target="_blank" href='http://rememberesc.com/'>http://rememberesc.com</a></div>
+                <div>생각솜씨 (국제 기억력 테스트 홈페이지) (퍼블리싱, 프론트엔드, 백엔드) (추가 개발 및 유지보수)</div>
+                <div>홈페이지 : <a target="_blank" href='http://15.165.13.245/'>http://15.165.13.245/</a></div>
             </div>
         </Mainprofile>
         <Mainprofile>
@@ -38,13 +41,14 @@ export const About = () => {
                 <p>논문</p>
                 <div>제목 : 불완전한 오버워치 게임결과 데이터를 이용한 인공신경망 기반 영웅 캐릭터 추천</div>
                 <div>설명 : 대학교 4학년때 인공지능 수강과목의 수업을 들으면서 인공지능이라는 학문에
-                관심을 가지게 되면서 인공지능이라는 학문을 프로그래밍과 연동을 하는 프로젝트를 하게 되었습니다.
+                    관심을 가지게 되면서 인공지능이라는 학문을 프로그래밍과 연동을 하는 프로젝트를 하게 되었습니다.
                     어느 날 교수님께서 제 프로젝트로 한국지능시스템학회의 학술지 논문 게제를 제안하셨고 저도
                     인공지능에 계속 관심을 가지고 싶어서 "오류 역전파 알고리즘"을 이용해서 당시 유행했던 게임
                     "오버워치"라는 소재를 활용해서 게임의 맵과 아군 팀의 캐릭터, 적 팀의 캐릭터의 이미지를
                     입력층으로 사용해서 그 상황에서 가장 선택하기 적합한 캐릭터 추천 프로그램을 만들었습니다.
                     그리고 이 논문은 현재 한국지능시스템학회 논문지 제27권 제3호, 2017.6, 268-275 (8 pages)에 게제되어있습니다.
-                    관련 링크 : <a target="_blank" href="https://www.dbpia.co.kr/journal/articleDetail?nodeId=NODE07187579&language=ko_KR">
+                    관련 링크 : <a target="_blank"
+                               href="https://www.dbpia.co.kr/journal/articleDetail?nodeId=NODE07187579&language=ko_KR">
                         https://www.dbpia.co.kr/journal/articleDetail?nodeId=NODE07187579&language=ko_KR
                     </a>
                 </div>
@@ -54,18 +58,30 @@ export const About = () => {
             <div>
                 <p>제가 가진 강점</p>
                 <div>
-                    프로젝트 혹은 역할 분담에 있어서 주도권을 가질 때 제 역량을 크게 발휘할 수 있습니다.
-                    논리학, 철학을 좋아해서 문제가 생겼을 때 문제의 본질에 대해서 이해하려고 하는 경향이 강합니다.
+                    모든 현상에 있어서 일정한 규칙을 찾는 속도가 보통사람들보다 빠릅니다. 그리고 그 규칙을 적절하게 응할 수 있는 능력을 가지고 있습니다.
                 </div>
-                <p>제가 가진 약점</p>
                 <div>
-                    프로젝트 혹은 역할 분담에 있어서 주도권을 잃을 경우 정해진 일 외 그 이상 창조적인 생각을 하지 않게 됩니다.
+                    다른 사람들보다 기억에 대한 실생활의 활용능력이 좋아서, 여러 현상에 오감에 대해서 기억해서 실생활의 적재적소에 활용할 수 있습니다.
+                </div>
+                <p>제가 가진 단점(부족한 점)</p>
+                <div>
+                    개발의 경우 현재까지 여러 프로젝트의 개발을 혼자 맡아서 여러 인원과 협업하는 개발 프로젝트일 경우 소통능력은 다소 부족합니다.
+                </div>
+                <div>
+                    완벽주의적 성격을 가지고 있습니다. 제 개인적으로는 장점이라고 생각하는데. 타인이 보기에는 작업속도가 느리게 보일 수 있어보게되고,
+                    고민만 하는 모습처럼 보게 되어서 걱정을 하게 됩니다.
                 </div>
                 <p>제가 가진 지향점</p>
                 <div>
                     프로젝트에 제 역할이 주어질 경우 저에게 있어서 가장 중요한 부분은 바로 개발하는 역할에서 제가 프로젝트의 주도권을 갖는가,
                     그렇지 않는가입니다. 남이 시키는 역할만 하는가 내가 주도적으로 하는가는 자기발전에 큰 차이가 존재합니다. 저는 후자를 원하고 앞으로도
                     후자의 역할군으로 개발을 하는 것을 원합니다.
+                </div>
+                <div>
+                    <div>1. 웹페이지 개발이 필요하신 분 (HTML, CSS, React, NodeJS, MongoDB) 기반의 웹페이지</div>
+                    <div>2. 웹페이지 유지보수가 필요하신 분 (개발 언어 필요 없음)</div>
+                    <div>3. (인건비 절약을 위한)자동화 프로그램이 필요하신 분</div>
+                    <div>위 조건에 의거하여 서비스가 필요하신 분들에게 소중한 사람이 되고 싶습니다.</div>
                 </div>
             </div>
         </Mainprofile>
