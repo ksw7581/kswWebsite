f07cec65bd48df4b2151caf32060d6ccb12d6de8
gimsang-won <ksw75811@gmail.com>
2025-06-17 13:12:51 +0900

    dev, prod 분리 테스트

--- index.ts
+++ index.ts
@@ -1,17 +1,25 @@
 const express = require("express");
 const path = require("path");
+const dotenv = require("dotenv");
 const app = express();
 const PORT = 80;
+dotenv.config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname)));
 
-app.get("*", function (req: any, res: { sendFile: (arg0: any) => void }, next: any) {
-    res.sendFile(path.join(__dirname, "index.html"));
+app.get("*", function(req: any, res: { sendFile: (arg0: any) => void }, next: any) {
+  const mode = process.env.mode;
+  console.log(mode);
+  if (mode === "development") {
+    res.sendFile(path.join(__dirname, "index.dev.html"));
+  } else {
+    res.sendFile(path.join(__dirname, "index.prod.html"));
+  }
 });
 
 app.listen(PORT, () => {
-    console.log(`
+  console.log(`
     ********************************
     *  Server started on port, ${PORT}  *
     ********************************
